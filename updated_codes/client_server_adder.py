26aaca2ab1e07049602ac7429bdbe0cf96f08ade
ShashwatNaidu <85156786+ShashwatNaidu@users.noreply.github.com>
2024-01-21 23:54:27 +0530

    updated codes

---
+++ updated_codes/client_server_adder.py
@@ -0,0 +1,25 @@
+# client.py
+
+import http.client
+import json
+
+def send_get_request(host='localhost', port=8000, path='/add'):
+    connection = http.client.HTTPConnection(host, port)
+    payload = {"n": 2, "hostnames": ["server1", "server2"]}
+    json_data = json.dumps(payload)
+    encoded_data = json_data.encode('utf-8')
+
+    headers = {'Content-Type': 'application/json',
+           'Content-Length': len(encoded_data)}
+    
+    connection.request('POST', path, body=encoded_data, headers=headers)
+    # connection.request('GET', path)
+    response = connection.getresponse()
+    print(f'Status: {response.status}')
+    print('Response:')
+    print(response.read().decode('utf-8'))
+    
+    connection.close()
+
+if __name__ == '__main__':
+    send_get_request()
---
+++ updated_codes/dummy_client.py
@@ -0,0 +1,25 @@
+# client.py
+
+import http.client
+import json
+
+def send_get_request(host='localhost', port=8000, path='/rm'):
+    connection = http.client.HTTPConnection(host, port)
+    payload = {"n": 1, "hostnames": []}
+    json_data = json.dumps(payload)
+    encoded_data = json_data.encode('utf-8')
+
+    headers = {'Content-Type': 'application/json',
+           'Content-Length': len(encoded_data)}
+    
+    connection.request('DELETE', path, body=encoded_data, headers=headers)
+    # connection.request('GET', path)
+    response = connection.getresponse()
+    print(f'Status: {response.status}')
+    print('Response:')
+    print(response.read().decode('utf-8'))
+    
+    connection.close()
+
+if __name__ == '__main__':
+    send_get_request()
